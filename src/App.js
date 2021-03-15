26a6ef4eb526088aef554f20928b3cc8849a8942
Zia Ur Rehman <ziaurr3hman@hotmail.com>
2021-03-15 14:31:50 +0500

    cleanup

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import Modal from './Modal';
 import Sidebar from './Sidebar';
 
 import Home from './Home';
--- src/Modal.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import { FaTimes } from 'react-icons/fa'
-const Modal = () => {
-  return <h2>Modal</h2>
-}
-
-export default Modal
